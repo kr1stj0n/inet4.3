8c07bf58f92c424277cb3a209d94cd557182ea16
kr1stj0n <kr1stj0n@pm.me>
2021-03-23 22:53:33 +0100

    adding ShQ files

---
+++ src/inet/queueing/filter/ShQDropper.cc
@@ -0,0 +1,154 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/queueing/filter/ShQDropper.h"
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/queueing/marker/EcnMarker.h"
+
+namespace inet {
+namespace queueing {
+
+Define_Module(ShQDropper);
+
+void ShQDropper::initialize(int stage)
+{
+    PacketFilterBase::initialize(stage);
+
+    if (stage == INITSTAGE_LOCAL) {
+        limit = par("limit");
+        if (limit < 0.0)
+            throw cRuntimeError("limit parameter must not be negative");
+        interval = par("interval");
+        if (interval < 0.0)
+            throw cRuntimeError("interval parameter must not be negative");
+        maxp = par("maxp");
+        if (maxp < 0.0 || maxp > 1.0)
+            throw cRuntimeError("Invalid value for maxp parameter: %g", maxp);
+        alpha = par("alpha");
+        if (alpha < 0.0 || alpha > 1.0)
+            throw cRuntimeError("Invalid value for alpha parameter: %g", alpha);
+        pkrate = par("pkrate");
+        if (pkrate < 0.0)
+            throw cRuntimeError("Invalid value for pkrate parameter: %g", pkrate);
+        useEcn = par("useEcn");
+        packetCapacity = par("packetCapacity");
+        auto outputGate = gate("out");
+        collection = findConnectedModule<IPacketCollection>(outputGate);
+        if (collection == nullptr)
+            collection = getModuleFromPar<IPacketCollection>(par("collectionModule"), this);
+
+        markingProbSignal = registerSignal("markingProb");
+        avgMarkingProbSignal = registerSignal("avgMarkingProb");
+        avgOutputRateSignal = registerSignal("avgOutputRate");
+    }
+}
+
+ShQDropper::ShQResult ShQDropper::doRandomEarlyDetection(const Packet *packet)
+{
+   double pb = 0.0;
+    int queueLength = collection->getNumPackets();
+
+    if (queueLength >= packetCapacity) { // maxth is also the "hard" limit
+        EV_DEBUG << "Queue length >= capacity" << EV_FIELD(queueLength) << EV_FIELD(packetCapacity) << EV_ENDL;
+        return QUEUE_FULL;
+    }
+
+    curRate += (double) packet->getByteLength();
+
+    const double ct = SIMTIME_DBL(simTime());
+    if (ct >= SIMTIME_DBL(r_time) + interval) {
+        double duration = ct - SIMTIME_DBL(r_time);
+        curRate += queueLength * 1500;
+        avgRate = (1.0 - alpha) * avgRate + alpha * curRate;
+        pb = maxp * (avgRate / (pkrate * duration));
+        if (pb < 0.0)
+            pb = 0;
+        else if (pb >= maxp)
+            pb = 1.0;
+
+        emit(avgMarkingProbSignal, pb);
+        emit(avgOutputRateSignal, avgRate * (interval / duration));
+
+        r_time = simTime();
+        curRate = 0;
+    }
+
+    if (dblrand() < pb) {
+        /* EV_DEBUG << "Random early packet detected" << EV_FIELD(averageQueueLength, avg) << EV_FIELD(probability, pb) << EV_ENDL; */
+        return RANDOMLY_MARK;
+    } else
+        return RANDOMLY_NOT_MARK;
+}
+
+bool ShQDropper::matchesPacket(const Packet *packet) const
+{
+    lastResult = const_cast<ShQDropper *>(this)->doRandomEarlyDetection(packet);
+    switch (lastResult) {
+        case RANDOMLY_MARK:
+            return useEcn && EcnMarker::getEcn(packet) != IP_ECN_NOT_ECT;
+        case RANDOMLY_NOT_MARK:
+            return true;
+        case QUEUE_FULL:
+            return false;
+        default:
+            throw cRuntimeError("Unknown ShQ result");
+    }
+}
+
+void ShQDropper::processPacket(Packet *packet)
+{
+    switch (lastResult) {
+        case RANDOMLY_MARK: {
+            if (useEcn) {
+                IpEcnCode ecn = EcnMarker::getEcn(packet);
+                if (ecn != IP_ECN_NOT_ECT) {
+                    // if next packet should be marked and it is not
+                    if (markNext && ecn != IP_ECN_CE) {
+                        EcnMarker::setEcn(packet, IP_ECN_CE);
+                        markNext = false;
+                    }
+                    else {
+                        if (ecn == IP_ECN_CE)
+                            markNext = true;
+                        else
+                            EcnMarker::setEcn(packet, IP_ECN_CE);
+                    }
+                }
+            }
+        }
+        case RANDOMLY_NOT_MARK:
+        case QUEUE_FULL:
+            return;
+        default:
+            throw cRuntimeError("Unknown ShQ result");
+    }
+}
+
+void ShQDropper::pushOrSendPacket(Packet *packet, cGate *gate,
+                                  IPassivePacketSink *consumer)
+{
+    PacketFilterBase::pushOrSendPacket(packet, gate, consumer);
+    // TD: Set the time stamp q_time when the queue gets empty.
+    const int queueLength = collection->getNumPackets();
+    if (queueLength == 0)
+        q_time = simTime();
+}
+
+} // namespace queueing
+} // namespace inet
+
---
+++ src/inet/queueing/filter/ShQDropper.h
@@ -0,0 +1,72 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_SHQDROPPER_H
+#define __INET_SHQDROPPER_H
+
+#include "inet/common/packet/Packet.h"
+#include "inet/queueing/base/PacketFilterBase.h"
+#include "inet/queueing/contract/IPacketCollection.h"
+
+namespace inet {
+namespace queueing {
+
+/**
+ * Implementation of Random Early Detection (RED).
+ */
+class INET_API ShQDropper : public PacketFilterBase
+{
+  protected:
+    enum ShQResult { QUEUE_FULL, RANDOMLY_MARK, RANDOMLY_NOT_MARK };
+
+  protected:
+    double limit = 0.0;
+    double interval = NaN;
+    double maxp = NaN;
+    double alpha = NaN;
+    double pkrate = NaN;
+
+    double avgRate = 0.0;
+    double curRate = 0.0;
+    simtime_t q_time;
+    simtime_t r_time;
+
+    int packetCapacity = -1;
+    bool useEcn = false;
+    bool markNext = false;
+    mutable ShQResult lastResult;
+
+    simsignal_t markingProbSignal;
+    simsignal_t avgMarkingProbSignal;
+    simsignal_t avgOutputRateSignal;
+
+    IPacketCollection *collection = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual ShQResult doRandomEarlyDetection(const Packet *packet);
+    virtual void processPacket(Packet *packet) override;
+    virtual void pushOrSendPacket(Packet *packet, cGate *gate, IPassivePacketSink *consumer) override;
+
+  public:
+    virtual bool matchesPacket(const Packet *packet) const override;
+};
+
+} // namespace queueing
+} // namespace inet
+
+#endif
